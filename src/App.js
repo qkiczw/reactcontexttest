284c456b0144d7398fa33fddf52d4f0f341ca152
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2023-01-04 18:25:29 +0100

    create a clean react test field.

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          Menu area 
+        </nav>
+      </header>
+      <main>
+        <div>
+          <h1>CONTEXT API TEST FIELD</h1>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
